534148295b013f92f1bfedce919781dad1c21006
PabloGLeiteDeSouza <pablo.guilherme.leite.de.souza@gmail.com>
2024-06-06 18:02:12 -0300

    "Updated AppController: changed type Teste properties, updated teste method to use senha instead of text"

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 type Teste = {
-  text: string;
+  login: string;
+  senha: string;
 }
 
 @Controller()
@@ -17,7 +18,7 @@ export class AppController {
   @Get("/teste")
   async teste(@Body() data: Teste): Promise<any> {
     console.log(data);
-    return await this.appService.teste(data.text);
+    return await this.appService.teste(data.senha);
   }
   // async teste1(@Query('text') text: string): Promise<any> {
   //   console.log(text);
